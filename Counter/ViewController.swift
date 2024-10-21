61c08846b4921db0dc76355f0d1f8e43f38c8840
Danila <danila.fafr@gmail.com>
2024-10-21 19:03:26 +0300

    Базовая задача

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -8,12 +8,19 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var numberUILabel: UILabel!
+    @IBOutlet weak var changeButton: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
+        numberUILabel.text = "Значение счётчика: \(number)"
         // Do any additional setup after loading the view.
     }
+   
+    private var number = 0
 
-
+    @IBAction func buttonDidTap(_ sender: Any) {
+       number += 1
+        numberUILabel.text = "Значение счётчика: \(number)"
+    }
 }
 
